c5205ee75eb9a1465e23531ad56480cf0f0913e9
村山光 <73589927+hikarumurayama@users.noreply.github.com>
2021-02-02 09:26:09 +0900

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
-import "./styles.css";
+const myProfile = {
+  name: "ひかる",
+  age: 25
+};
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+//${}の中身が冗長でわかりにくい
+const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}です。`;
+console.log(message1);
+
+//分割代入
+const { name, age } = myProfile;
+
+//すっきり書くことができた
+const message2 = `名前は${name}です。年齢は${age}です。`;
+console.log(message2);
+
+const myProfile２ = ["ひかる", 25];
+
+// indexで指定する場合
+const message3 = `名前は${myProfile２[0]}です。年齢は${myProfile２[1]}です`;
+console.log(message3);
+
+// 分割代入
+const [name２, age２] = myProfile２;
+const message4 = `名前は${name２}です。年齢は${age２}です。`;
+console.log(message4);
