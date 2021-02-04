7d6a0d956fe7a6b974f8b8df6537cac5b5df2c5d
村山光 <73589927+hikarumurayama@users.noreply.github.com>
2021-02-04 22:54:47 +0900

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,26 +1,16 @@
-const myProfile = {
-  name: "ひかる",
-  age: 25
-};
+/* map*/
+const nameArr = ["田中", "山田", "大田"];
+//nameArr.map((name) => console.log(name));
 
-//${}の中身が冗長でわかりにくい
-const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}です。`;
-console.log(message1);
+/* filter*/
+const numArr = [1, 2, 3, 4, 5];
+const oddNums = numArr.filter((num) => {
+  return num % 2 === 1;
+});
+//console.log(oddNums)
 
-//分割代入
-const { name, age } = myProfile;
-
-//すっきり書くことができた
-const message2 = `名前は${name}です。年齢は${age}です。`;
-console.log(message2);
-
-const myProfile２ = ["ひかる", 25];
-
-// indexで指定する場合
-const message3 = `名前は${myProfile２[0]}です。年齢は${myProfile２[1]}です`;
-console.log(message3);
-
-// 分割代入
-const [name２, age２] = myProfile２;
-const message4 = `名前は${name２}です。年齢は${age２}です。`;
-console.log(message4);
+/**index */
+const member = ["taro", "takashi", "youta"];
+member.map((name, index) => {
+  //  console.log(`私の名前は${name}です。背が${index}番目に高いです`)
+});
